bfc044ebe4c67583fd7b2d6e461ecd8c86da97d4
Miłosz <milpie51@gmail.com>
2025-04-15 12:36:10 +0200

    Fixed generate additional data function logic

--- functions/generate_additional_data.R
+++ functions/generate_additional_data.R
@@ -10,34 +10,35 @@ generate_additional_data <- function(data, indicators) {
     mutate(BMI = round(weight / ((height / 100) ^ 2), 2))
   
   # Oblicz poziom edukacji według ISCED wykorzystując rowwise do indywidualnego losowania dla każdego wiersza
-  data <- data %>% 
-    rowwise() %>% 
-    mutate(education = case_when(
-      age < 3 ~ "0",   # przedział: <3 lata
-      age >= 3 & age <= 5 ~ 
-        sample(c("0", "None"), 1, 
-               prob = c(indicators$age_indicator_kindergarten, indicators$age_indicator_nokindergarten)),
-      age >= 6 & age <= 11 ~ 
-        sample(c("1", "None"), 1, 
-               prob = c(indicators$age_indicator_primary, indicators$age_indicator_noprimary)),
-      age >= 12 & age <= 15 ~ 
-        sample(c("2", "None"), 1, 
-               prob = c(indicators$age_indicator_lowersecondary, indicators$age_indicator_nolowersecondary)),
-      age >= 16 & age <= 18 ~ 
-        sample(c("3", "4", "None"), 1, 
-               prob = c(indicators$age_indicator_uppersecondary, indicators$age_indicator_postsecondary,
-                        indicators$age_indicator_nouppersecondary)),
-      age >= 19 & age <= 20 ~ 
-        sample(c("4", "6", "None"), 1, 
-               prob = c(indicators$age_indicator_postsecondary, indicators$age_indicator_bachelor,
-                        indicators$age_indicator_nohigher)),
-      age >= 21 ~ 
-        sample(c("6", "7", "8", "None"), 1, 
-               prob = c(indicators$age_indicator_bachelor, indicators$age_indicator_master, 
-                        indicators$age_indicator_doctorate, indicators$age_indicator_nohigher)),
-      TRUE ~ NA_character_
-    )) %>% 
+  data <- data %>%
+    rowwise() %>%
+    mutate(
+      education = {
+        ind <- indicators[[country]]  # pobieramy zestaw współczynników dla kraju
+        
+        if (age < 3) {
+          "0"  # edukacja przedszkolna – dzieci przed systemem formalnym
+        } else if (age >= 3 & age <= 5) {
+          sample(c("0", "None"), 1, prob = c(ind$age_indicator_kindergarten, ind$age_indicator_nokindergarten))
+        } else if (age >= 6 & age <= 11) {
+          sample(c("1", "None"), 1, prob = c(ind$age_indicator_primary, ind$age_indicator_noprimary))
+        } else if (age >= 12 & age <= 14) {
+          sample(c("2", "None"), 1, prob = c(ind$age_indicator_lowersecondary, ind$age_indicator_nolowersecondary))
+        } else if (age >= 15 & age <= 17) {
+          sample(c("3", "4", "None"), 1, prob = c(ind$age_indicator_uppersecondary, ind$age_indicator_postsecondary, ind$age_indicator_nouppersecondary))
+        } else if (age >= 18 & age <= 20) {
+          sample(c("4", "5", "6", "None"), 1, prob = c(ind$age_indicator_postsecondary, ind$age_indicator_postsecondary, ind$age_indicator_bachelor, ind$age_indicator_nohigher))
+        } else if (age >= 21 & age <= 24) {
+          sample(c("6", "7", "None"), 1, prob = c(ind$age_indicator_bachelor, ind$age_indicator_master, ind$age_indicator_nohigher))
+        } else if (age >= 25) {
+          sample(c("6", "7", "8", "None"), 1, prob = c(ind$age_indicator_bachelor, ind$age_indicator_master, ind$age_indicator_doctorate, ind$age_indicator_nohigher))
+        } else {
+          NA_character_
+        }
+      }
+    ) %>%
     ungroup()
+  
   
   # Dodaj kolumnę citizenship – losuje dla każdego wiersza
   n <- nrow(data)
